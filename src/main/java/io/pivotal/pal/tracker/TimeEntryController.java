c2f5be848668da5ac4ada5f5ceb05ec18efea8d9
Josh Herndon <jherndon80@gmail.com>
2020-11-06 17:37:50 +0000

    Lab Database Migrations

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import java.util.List;
 @RestController
 public class TimeEntryController {
 
+    //@Autowired
     private TimeEntryRepository repo;
 
     public TimeEntryController(TimeEntryRepository repo){
